6ae8a942056211e47d086e3a3f799f0d3ff11a58
amirbraham <amirbrahamm@gmail.com>
2023-03-15 14:11:47 +0100

    adding firefox binary path

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import os
 import json
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
 # ------------CONFIG START ----------------------
 HEADLESS = True
 if ("ON_HEROKU" in os.environ):
@@ -44,6 +46,8 @@ else:
 # ------------CONFIG END ----------------------
 
 
+
+
 options = webdriver.FirefoxOptions()
 if (HEADLESS):
     options.add_argument("--headless=new")
@@ -53,8 +57,12 @@ if (("ON_HEROKU" in os.environ)):
     # might not be needed
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument('--window-size=1920x1480')
+binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
 
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox(
+		firefox_binary=binary,
+		executable_path=os.environ.get('GECKODRIVER_PATH'),
+		options=options)
 
 driver.get("https://logement.cesal-residentiel.fr/espace-resident/cesal_login.php")
 
